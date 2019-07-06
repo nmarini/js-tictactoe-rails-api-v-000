8b9b0a9010b982a39f55d9a8be9e83b7d566bf65
Nicolas Marini <nmarini44@gmail.com>
2019-07-06 17:46:54 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -29,7 +29,7 @@ function checkWinner() {
   var board = {};
   var winner = false;
 
-  $("td").text(function(index, square) { board[index] => square });
+  $('td').text((index, square) => board[index] = square);
 
   WINNING_COMBOS.some(function(combo) {
     if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
@@ -37,4 +37,53 @@ function checkWinner() {
       return winner = true;
     }
   });
+  return winner;
+}
 
+function doTurn(square) {
+  updateState(square);
+  turn++;
+
+  if (checkWinner()) {
+  // resetGame() -- resets board and turn
+    saveGame();
+    resetGame();
+  } else if (turn === 9) {
+    setMessage("Tie game.");
+    saveGame();
+    resetGame();
+  }
+
+}
+
+function resetGame() {
+  turn = 0;
+  $('td').empty();
+  currentGame = 0;
+}
+
+function saveGame() {
+  var state = [];
+  var gameData;
+
+  $('td').text((index, square) => {
+    state.push(square)
+  });
+
+  gameData = { state: state };
+
+  if (currentGame) {
+    $.ajax({
+      url: `/games/${currentGame}`,
+      type: 'PATCH',
+      data: gameData
+    })
+  } else {
+    $.post(`/games`, gameData, function(game) {
+      currentGame = game.data.id;
+      $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`)
+      $(`gameid-` + game.data.id).on('click', () => reloadGame(game.data.id));
+    });
+  }
+}
+// CREATE reloadGame() THAT RELOADS THE GAME OF THE GAME_ID THAT IS PASSED THROUGH
